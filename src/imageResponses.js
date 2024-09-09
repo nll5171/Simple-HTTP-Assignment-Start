8eb29a71664db9e25f7313e81b888f567fefa394
Nick Lang <nll5171@rit.edu>
2024-09-08 22:56:38 -0400

    Added comment to test with GitHub Actions

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs'); // pull in the file system module
 
 const spongegar = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
 
+// spongegar
 const getMeme = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'image/png' });
   response.write(spongegar);
